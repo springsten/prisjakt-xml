779657d59dfe46ecb7358d36b3ed3c8a3aae72e0
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-05-22 13:43:24 +0200

    Initialize project with package configuration, dotenv integration, and database connection setup

---
+++ src/database/connection.ts
@@ -0,0 +1,13 @@
+import { createPool } from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+export const connection = pool.getConnection();
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+const getAllProducts = () => {};
+
+console.log("HEJ!");
+
+// Hämta alla produkter från sql
+// skicka in till XML funcition
+// mappa igenom produkter -> lägger varje produkt i i XML modellen
